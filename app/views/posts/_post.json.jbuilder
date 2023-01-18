690bfcbb904b9be3f97f025f9f7f8409ea670125
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2023-01-19 01:29:16 +0900

    chore: add devise, action text and migrate db

---
+++ app/views/posts/_post.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! post, :id, :title, :posted_at, :changed_at, :created_at, :updated_at
+json.url post_url(post, format: :json)
---
+++ db/migrate/20230118135724_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, limit: 100
+      t.datetime :posted_at
+      t.datetime :changed_at
+
+      t.timestamps
+    end
+  end
+end
