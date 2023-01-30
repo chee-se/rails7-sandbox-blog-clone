5b2f050596da7dd1a3b97fd2db0fb6a42b92a467
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2023-01-31 02:49:07 +0900

    feat: change routing setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
-  devise_for :administrators, module: 'administrators'
-  devise_for :users, module: 'users'
+  devise_for :administrators, module: :administrators
+  devise_for :users, module: :users
+
+  unauthenticated do
+    root 'posts#index'
+  end
+
+  authenticated :administrators do
+    root 'administrators#index', as: :administrator_root
+  end
+
+  authenticated :users do
+    root 'posts#index', as: :user_root
+  end
 
   resources :administrators
   resources :posts
+  resources :users
 
-  root 'posts#index'
 end
