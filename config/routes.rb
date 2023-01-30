6dbe3c662d23a02c7ddd502661dd51c059067d59
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2023-01-30 17:43:02 +0900

    feat: remove unuse model for remodel

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  draw :devise
   resources :posts
-  resources :admins
-
-  namespace :admin do
-    resources :users
-  end
 
   root 'posts#index'
 end
--- config/routes/devise.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  devise_for :users, module: 'devise/users'
-  devise_for :admins, module: 'devise/admins'
-end
